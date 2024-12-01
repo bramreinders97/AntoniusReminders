f54392ba61898e86a3d7231d417dc6864846660c
Bram Reinders <bram.reinders@blenddata.nl>
2024-12-01 16:48:24 +0100

    create working logic

--- telegram.py
+++ telegram.py
@@ -1,29 +1,30 @@
-import requests
-import os
+import os.path
+from datetime import datetime
 
-# Replace with your Bot Token
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+from send_msg import send_msg
+from check_if_need_msg import is_message_date
+import json
 
-# Replace with your Chat ID
-CHAT_ID = os.getenv('CHAT_ID')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "messages.json")
 
-if BOT_TOKEN is None:
-    print('BOT_TOKEN not set')
+# Load the JSON file containing message info
+with open(file_path, 'r') as file:
+    message_data = json.load(file)
 
-# Message to send
-MESSAGE = "Hello, this is a message from my Python script!"
-
-# Telegram API URL
-URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-# Sending the message
-response = requests.post(URL, data={
-    "chat_id": CHAT_ID,
-    "text": MESSAGE
-})
-
-# Checking the response
-if response.status_code == 200:
-    print("Message sent successfully!")
-else:
-    print("Failed to send message:", response.text)
+# Check if we need to send a message today
+# If so, do it
+for kind, msg_info in message_data.items():
+    if is_message_date(
+        check_date_start=datetime(
+            msg_info["start_date_year"],
+            msg_info["start_date_month"],
+            msg_info["start_date_day"],
+        ),
+        msg_every_x_weeks=msg_info["msg_every_x_weeks"]
+    ):
+        send_msg(
+            msg=msg_info["msg"],
+        )
+    else:
+        print(f"No need to send msg for reminder on {kind}")

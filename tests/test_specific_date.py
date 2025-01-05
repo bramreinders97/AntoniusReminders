136ae53d986c38b5437afc513e3bb4e688878d6b
BramReinders <bram.reinders@blenddata.nl>
2025-01-05 14:09:25 +0100

    formatting

--- tests/test_specific_date.py
+++ tests/test_specific_date.py
@@ -2,11 +2,14 @@
 
 import time
 from datetime import datetime
+from tkinter import Button, Label, Tk
+
 import requests
-from tkinter import Tk, Button, Label
 from tkcalendar import Calendar
+
 from src.antonius_reminders.AntoniusRemindersBot import AntoniusRemindersBot
 from src.antonius_reminders.constants import BOT_TOKEN, CHAT_ID
+
 
 def send_single_msg(text: str) -> None:
     """Send a single message in telegram."""
@@ -15,14 +18,16 @@ def send_single_msg(text: str) -> None:
         data={"chat_id": CHAT_ID, "text": text, "parse_mode": "MarkdownV2"},
     )
 
+
 def send_messages_for_dates(selected_dates):
     """Send messages for the selected dates."""
     reminders_bot = AntoniusRemindersBot()
     for date_str in selected_dates:
-        test_date = datetime.strptime(date_str, '%Y-%m-%d')
+        test_date = datetime.strptime(date_str, "%Y-%m-%d")
         send_single_msg(text=f"{test_date.strftime('%d %B %Y')}:")
         reminders_bot.send_messages_of_the_day(date=test_date)
         time.sleep(5)  # To avoid too many requests error
+
 
 def open_calendar():
     """Open a calendar for selecting multiple dates."""
@@ -44,7 +49,7 @@ def open_calendar():
     root.title("Select Dates")
 
     Label(root, text="Select dates for sending messages:").pack(pady=10)
-    calendar = Calendar(root, selectmode='day', date_pattern='yyyy-MM-dd')
+    calendar = Calendar(root, selectmode="day", date_pattern="yyyy-MM-dd")
     calendar.pack(pady=10)
 
     selected_label = Label(root, text="Selected Dates: None")
@@ -54,6 +59,7 @@ def open_calendar():
     Button(root, text="Done", command=on_done).pack(pady=10)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     open_calendar()
